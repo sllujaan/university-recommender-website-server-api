d73763b5fa6d239543f8fbd70c9220dcb56706b7
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-06 14:58:34 +0500

    bugs fixes.

--- util/response.php
+++ util/response.php
@@ -1,13 +1,36 @@
 <?php
 
 
+
+function prepareMsg($statusText, $msg) {
+    return "<h1>$statusText</h1><p>$msg</p>";
+
+}
+
+function getMessage($statusCode) {
+    $msg = "";
+    switch ($statusCode) {
+        case 400:
+            return prepareMsg("Bad Request", "You don't have permission to access this resource.");
+        default:
+            return "";
+            break;
+    } 
+}
+
+
 function sendResponseStatus($statusCode) {
     $sapitype = php_sapi_name();
-    if (substr($sapitype, 0, 3) === 'cgi')
+    if (substr($sapitype, 0, 3) === 'cgi') {
+        echo getMessage($statusCode);
         return header("Status: " . $statusCode); 
-    else
+    }
+    else {
+        echo getMessage($statusCode);
         return header("HTTP/1.1 " . $statusCode);
+    }
 }
+
 
 
 
