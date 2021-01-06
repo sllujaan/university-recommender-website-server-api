120ace729ad48b06b40dbe69d33b71cff24d6772
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-06 16:43:30 +0500

    bugs fixes.

--- DB/connection.php
+++ DB/connection.php
@@ -1,18 +1,19 @@
 <?php
     include_once("credential.php");
     
+    
     function initConnection($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE) {
         $mysqli = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE);
 
         // Check connection
         if ($mysqli->connect_errno) {
-            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            echo "Failed to connect to MySQL: " . $mysqli->connect_error;
             exit();
         }
 
         echo "new connection created.";
+        return $mysqli;
     }
-
-    initConnection(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+    
 
 ?>
--- DB/validator.php
+++ DB/validator.php
@@ -2,8 +2,23 @@
     include_once("credential.php");
     include_once("connection.php");
 
-    //echo $MYSQL_HOST;
 
-    //initConnection();
+    $conn = initConnection(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+    $sql = "select * from role";
+
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<br>name: ". $row["Name"] . "<br>";
+        }
+    }
+    else {
+        echo "0 results";
+    }
+
+
+
+    $conn->close();
 
 ?>
