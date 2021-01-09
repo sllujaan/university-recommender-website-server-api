46771f73df945b99e6f803654ec95fccb9cae065
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-09 16:26:52 +0500

    bugs fixes.

--- DB/addNewData.php
+++ DB/addNewData.php
@@ -16,6 +16,12 @@
 
         $row = $result->fetch_assoc();
         $id = $row[$columnName];
+        //check that id is not empty. make columnName is correct
+        if(empty($id)) {
+            sendResponseStatus(500);
+            exit();
+        }
+
         return $id;
     }
 
@@ -24,9 +30,64 @@
         //create new connection
         $conn = initConnection();
 
-        //retrieve normal User Role_ID
-        $sql = "select (Role_IDa) from Role where Name = 'user';";
+        //retrieve Role_ID for normal users
+        $sql = "select (Role_ID) from Role where Name = 'user';";
         $UserRoleID = getSingleColumn($conn, $sql, "Role_ID");
+        //retrieve Account_Status_ID for normal users
+        $sql = "select (Account_Status_ID) from Account_Status where Name = 'pending';";
+        $AccountSatatusID = getSingleColumn($conn, $sql, "Account_Status_ID");
+
+
+        
+
+        $stmt = $conn->prepare("
+            insert into User (
+            `Role_ID`,
+            `Account_Status_ID`,
+            `Name`,
+            `Password`,
+            `Email`,
+            `Start_Admission_Date`,
+            `Budget`,
+            `S_Education_PCT`,
+            `H_Education_PCT`,
+            `ETM_PCT`
+            )
+            values
+            (
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?,
+                ?
+            )"
+        );
+
+        $stmt->bind_param(
+            "iissssiddd", $UserRoleID, $AccountSatatusID,
+            $_POST["name"], $_POST["password"], $_POST["email"],
+            $_POST["start_admission_date"], $_POST["budget_us_$"], $_POST["s_education_pct"],
+            $_POST["h_education_pct"], $_POST["etm_pct"]
+        );
+
+        //$stmt->execute();
+        if(!$stmt->execute()) {
+            sendResponseStatus(500);
+            echo "Failed to add the Record: " . $conn->error;
+            exit();
+        }
+        
+        $result = $stmt->get_result();
+
+        echo $_POST["name"];
+
+
+        
 
 
         //close the connection
