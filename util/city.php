54d8f2c2740ecb7c99db7aeb54dc7bb4a962843d
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-24 13:04:28 +0500

    cors bugs fixes.

--- util/city.php
+++ util/city.php
@@ -1,4 +1,5 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
 
     include_once("response.php");
     include_once("../DB/getData.php");
--- util/country.php
+++ util/country.php
@@ -1,5 +1,6 @@
 <?php
-
+    header("Access-Control-Allow-Origin: *");
+    
     include_once("response.php");
     include_once("../DB/getData.php");
 
