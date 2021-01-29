3569887eb96927d9ed4ad2685816e837000456f3
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-01-29 11:44:02 +0500

    commenting code.

--- util/city.php
+++ util/city.php
@@ -4,6 +4,10 @@
     include_once("response.php");
     include_once("../DB/getData.php");
 
+
+    /**
+     * checks if country id is present in the request.
+     */
     if(!isset($_GET['id'])) {
         //no id found in url
         sendResponseStatus(400);
@@ -11,16 +15,19 @@
         exit();
     }
 
+    /**
+     * checks if the country id in the request is a number.
+     */
     if(filter_var($_GET['id'], FILTER_VALIDATE_INT) === false) {
         sendResponseStatus(400);
         echo "Invalid id in URL";
         exit();
     }
 
+
+    /**
+     * retrive the cities from the country id and print as json.
+     */
     echo \DATABASE_GET_DATA\getCities($_GET['id']);
-
-
-    sendResponseStatus(200);
-    exit();
 
 ?>
--- util/response.php
+++ util/response.php
@@ -1,49 +1,53 @@
 <?php
 
 
+    /**
+     * generates message to display on web browsers.
+     */
+    function prepareMsg($statusText, $msg) {
+        return "<h1>$statusText</h1><p>$msg</p>";
 
-function prepareMsg($statusText, $msg) {
-    return "<h1>$statusText</h1><p>$msg</p>";
-
-}
-
-function getMessage($statusCode) {
-    $msg = "You don't have permission to access this resource.";
-    switch ($statusCode) {
-        case 400:
-            return prepareMsg("Bad Request", $msg);
-        case 401:
-            return prepareMsg("Unauthorized", $msg);
-        case 408:
-            return prepareMsg("Request Timeout", $msg);
-        case 404:
-            return prepareMsg("Not Found", $msg);
-        case 409:
-            return prepareMsg("Conflict", $msg);
-        case 500:
-            return prepareMsg("Internal Server Error", "Please contact to Administrator!");
-        default:
-            return "";
-            break;
-    } 
-}
-
-
-function sendResponseStatus($statusCode) {
-    $sapitype = php_sapi_name();
-    if (substr($sapitype, 0, 3) === 'cgi') {
-        header("Access-Control-Allow-Origin: *");
-        header("Status: " . $statusCode);
-        echo getMessage($statusCode);
     }
-    else {
-        header("Access-Control-Allow-Origin: *");
-        header("HTTP/1.1 " . $statusCode);
-        echo getMessage($statusCode);
+
+    /**
+     * generates appropriate message from http status codes.
+     */
+    function getMessage($statusCode) {
+        $msg = "You don't have permission to access this resource.";
+        switch ($statusCode) {
+            case 400:
+                return prepareMsg("Bad Request", $msg);
+            case 401:
+                return prepareMsg("Unauthorized", $msg);
+            case 408:
+                return prepareMsg("Request Timeout", $msg);
+            case 404:
+                return prepareMsg("Not Found", $msg);
+            case 409:
+                return prepareMsg("Conflict", $msg);
+            case 500:
+                return prepareMsg("Internal Server Error", "Please contact to Administrator!");
+            default:
+                return "";
+                break;
+        } 
     }
-}
 
-
-
+    /**
+     * sets response header from http status code.
+     */
+    function sendResponseStatus($statusCode) {
+        $sapitype = php_sapi_name();
+        if (substr($sapitype, 0, 3) === 'cgi') {
+            header("Access-Control-Allow-Origin: *");
+            header("Status: " . $statusCode);
+            echo getMessage($statusCode);
+        }
+        else {
+            header("Access-Control-Allow-Origin: *");
+            header("HTTP/1.1 " . $statusCode);
+            echo getMessage($statusCode);
+        }
+    }
 
 ?>
