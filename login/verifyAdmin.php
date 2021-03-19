fd105c92a3c613354f664dcbffe4b3c957cdaf53
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2021-03-19 11:25:47 +0500

    added verify admin api

--- login/verifyAdmin.php
+++ login/verifyAdmin.php
@@ -3,7 +3,8 @@
     include_once("../util/header.php");
     include_once("../util/validator.php");
     include_once("../util/session.php");
-    
+    include_once("../DB/connection.php");
+    include_once("../DB/validator.php");
 
     /*make sure that the incoming request is a post request*/
     \UTIL_VALIDATOR\validatePostRequest();
@@ -13,5 +14,13 @@
 
     /*make sure that the session exists for the incoming user's request*/
     \SESSION\validateSession_POST();
+
+    //create new connection
+    $conn = initConnection();
+
+    \DATABASE_VALIDATOR\verifyAdmin($conn, $_POST["user_id"]);
+
+    //close the connection
+    $conn->close();
 
 ?>
